1b115b69179345c7a94919489e8834660e9c3e79
xiaohuwu <1205150638@qq.com>
2020-07-14 03:29:47 +0800

    fix bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,11 +24,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
-	end
-  logger = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.log_level = :debug
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
